b1e0ecbc7f906e0c34ecd5da3d05fca85ae6adc0
Jean-Christian Denis <contact@jcdenis.fr>
2023-07-25 22:39:54 +0200

    release 0.4

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Dotclear Watch',
     'Send report about your Dotclear',
     'Jean-Christian Denis and contributors',
-    '0.3',
+    '0.4',
     [
         'requires' => [
             ['php', '7.4'],
